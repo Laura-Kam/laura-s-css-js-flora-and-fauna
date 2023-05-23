5b09d86ab331a2f5722aa0b0e65ae64a74015aa9
Laura Kambanis <kambanislaura@gmail.com>
2023-05-23 19:11:17 +0100

    added on hover effect to links, practiced .textContent, solved but by using [0] index 0 when targetting the element with a class, solved a bug by removing margin-top from main to close gap, removed default browser styling by adding margin-top 0 to body

--- index.js
+++ index.js
@@ -1,61 +1,43 @@
-console.log("It works chill");
-
-// function myFunction() {
-//   var x = document.getElementById("myDIV");
-//   if (x.style.display === "none") {
-//     x.style.display = "block";
-//   } else {
-//     x.style.display = "none";
-//   }
-// }
-// declaring variables from ids
-
-// const questionBtn = document.getElementById("questionButton");
-// const answerP = document.getElementById("answerParagraph");
-
-// //create an event listener.
-
-// questionBtn.addEventListener("click", function showAnswer() {
-//   //the problem here was I have to start with 'none'
-//   if (answerP.style.display == "none") {
-//     answerP.style.display = "block";
-//   } else {
-//     answerP.style.display = "none";
-//   }
-// });
-
-//The error here was I was a comparison operator, not an assignment operator to change display property.
-const asideQuestion = document.getElementById("asideQuestion");
-const asideAnswer = document.getElementById("asideAnswer");
-
-asideQuestion.addEventListener("click", function revealAnswer() {
-  if (asideAnswer.style.display === "none") {
-    asideAnswer.style.display = "block";
-  } else {
-    asideAnswer.style.display = "none";
-  }
-});
+//Query selectors and direct child combinators
 
 firstSibling = document.querySelector("#parentElement > p");
 
-secondSibling = firstSibling.nextElementSibling;
+//Practising nextElementSiblings
+// secondSibling = firstSibling.nextElementSibling;
 
-console.log(secondSibling);
+// console.log(secondSibling);
 
-const first = document.querySelector(".first");
+// const first = document.querySelector(".first");
 
-const findClassValue = first.getAttribute("class");
+// const findClassValue = first.getAttribute("class");
 
-console.log(findClassValue);
+// console.log(findClassValue);
 
-const child = document.getElementsByClassName("first")[0]; // Access the first element with the class "first"
+// const child = document.getElementsByClassName("first")[0]; // Access the first element with the class "first"
 
-const mySibling = child.nextElementSibling; // Retrieve the next sibling element
+// const mySibling = child.nextElementSibling; // Retrieve the next sibling element
 
-mySibling.setAttribute("class", "this-is-the-second-another-list-item"); // Set the class attribute of the sibling element
+// mySibling.setAttribute("class", "this-is-the-second-another-list-item"); // Set the class attribute of the sibling element
 
-mySibling.textContent = "Learning";
+//Practising textContent
+// mySibling.textContent = "Learning";
 
-const item = document.getElementById("second-article");
-const value = item.childNodes[2].nodeValue;
-console.log(value);
+// const item = document.getElementById("second-article");
+// const value = item.childNodes[2].nodeValue;
+// console.log(value);
+
+//Practising textContent part 2 - changed nav item one to blog posts using the DOM.
+
+//Index 0 needed as otherwise textContent targets a collection of elements
+// const navItemOne = document.getElementsByClassName("main-nav__item1")[0];
+// navItemOne.textContent = "Blog Posts";
+
+// console.log(
+//   "This is what my textContent has targetted with index 0: " +
+//     navItemOne.textContent
+// );
+
+//Practising textContent part 3 - changed nav item one to blog posts using the DOM.
+
+// const navItem2 = document.getElementsByClassName("main-nav__item2")[0];
+// navItem2.textContent = "JS Projects";
